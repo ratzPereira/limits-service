378c6d80867e3ae7d32cf5a2a5e6f7778f032fb4
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-20 16:53:30 +0000

    created hard coded limit controller and service

--- src/main/java/ratz/microservices/controller/LimitsConfigurationController.java
+++ src/main/java/ratz/microservices/controller/LimitsConfigurationController.java
@@ -1,4 +1,17 @@
 package ratz.microservices.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ratz.microservices.bean.LimitsConfiguration;
+
+@RestController
 public class LimitsConfigurationController {
+
+
+    @GetMapping("/limits")
+    public LimitsConfiguration retrieveLimitsFromConfigurations(){
+
+        return new LimitsConfiguration(1000,1);
+    }
 }
