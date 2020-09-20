c73c62aa57ffda2a368533830c9f442093cb42dd
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-20 17:07:19 +0000

    created an application configuration file and COnfiguration class to pickup values from app.properties

--- src/main/java/ratz/microservices/controller/LimitsConfigurationController.java
+++ src/main/java/ratz/microservices/controller/LimitsConfigurationController.java
@@ -3,15 +3,25 @@ package ratz.microservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ratz.microservices.Configuration;
 import ratz.microservices.bean.LimitsConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
 
 
+    private Configuration configuration;
+
+
+    public LimitsConfigurationController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+
+
     @GetMapping("/limits")
     public LimitsConfiguration retrieveLimitsFromConfigurations(){
 
-        return new LimitsConfiguration(1000,1);
+        return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
     }
 }
